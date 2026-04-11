d02ebf5922cfb8debe60a32f4f919b2a684c4d9a
sreyesflorez0 <sreyesflorez0@gmail.com>
2026-04-11 12:15:17 -0500

    Agregue clases de exception

---
+++ user-service/src/main/java/com/ecommerce/user/repository/UserProfileRepository.java
@@ -0,0 +1,13 @@
+package com.ecommerce.user.repository;
+
+import com.ecommerce.user.entity.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserProfileRepository extends JpaRepository<UserProfile, UUID> {
+    Optional<UserProfile> findByUserId(UUID userId);
+    boolean existsByUserId(UUID userId);
+}
